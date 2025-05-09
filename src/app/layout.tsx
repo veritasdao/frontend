57ac8c1bb7c618c3e7ea069ed7e6e05cff683dfe
zakyirsyaad <zkyxentertain@gmail.com>
2025-05-10 03:29:32 +0700

    feat: update layout metadata and favicon for improved SEO and branding
    - Added Open Graph metadata to enhance social media sharing with relevant title, description, and image.
    - Included authors and keywords in the metadata for better search engine optimization.
    - Updated favicon to align with the new branding.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,30 @@ export const metadata: Metadata = {
   title: "Veritas DAO",
   description:
     "Veritas is a web3 DAO crowdfunding and public funding platform based on donations built on the LISK network with IDRX token.",
+  openGraph: {
+    title: "Veritas DAO",
+    description:
+      "Veritas is a web3 DAO crowdfunding and public funding platform based on donations built on the LISK network with IDRX token.",
+    url: "https://www.veritasdao.xyz",
+    type: "website",
+    images: [
+      {
+        url: "https://brown-glamorous-bear-418.mypinata.cloud/ipfs/bafkreiewce7qrp3hpg3fagiq6fegtcyzkkxkfhfsbfcdo2rh5qjgii5qiu", // URL gambar untuk Open Graph
+        alt: "Veritas DAO",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  authors: [
+    {
+      name: "Kazuya Team",
+      url: "https://www.veritasdao.xyz",
+    },
+  ],
+
+  keywords:
+    "crowdfunding, public funding, DAO, LISK, IDRX, donations, web3, blockchain, crowdfunding platform, public funding platform, DAO platform, LISK network, IDRX token, donations platform, crowdfunding platform, public funding platform, DAO platform, LISK network, IDRX token, donations platform",
 };
 
 export default function RootLayout({
